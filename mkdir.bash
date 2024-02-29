263a187d7a71b841ad8e30c90c30630215e73667
alkaline2018 <plus159@>
2024-02-29 10:37:00 +0900

    init: start

---
+++ mkdir.bash
@@ -0,0 +1,11 @@
+mkdir -p grafana/provisioning
+mkdir -p grafana/data
+mkdir -p proprometheus/config
+mkdir -p proprometheus/data
+
+cd proprometheus/config
+# prometheus-start.yml
+# prometheus.yml
+# prometheus-with-exporter.yml
+
+# data sources
